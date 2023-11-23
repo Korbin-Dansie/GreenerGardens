52ff563b42ab364428c4f9cee1bbe4cc25fb6223
Korbin Dansie <dansiekm@gmail.com>
2023-11-22 17:18:52 -0700

    Add bootstrap

--- GreenerGardens/settings.py
+++ GreenerGardens/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/http://127.0.0.1:8000/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
